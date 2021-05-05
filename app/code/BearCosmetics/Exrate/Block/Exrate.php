aadbc16440f78fd42d0845ffdeb720898e7a583c
Hung <hung291100@gmail.com>
2021-05-05 21:24:43 +0700

    added exrate

--- app/code/BearCosmetics/Exrate/Block/Exrate.php
+++ app/code/BearCosmetics/Exrate/Block/Exrate.php
@@ -2,8 +2,8 @@
 
 namespace BearCosmetics\Exrate\Block;
 
-class Feed extends \Magento\Framework\View\Element\Template {
-    const FEED_URL = "https://vnexpress.net/rss/kinh-doanh.rss";
+class Exrate extends \Magento\Framework\View\Element\Template {
+    const FEED_URL = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx?b=68";
 
     
     protected function _prepareLayout()
@@ -11,11 +11,11 @@ class Feed extends \Magento\Framework\View\Element\Template {
         return parent::_prepareLayout();
     }
 
-    public function fetchFeed() {
+    public function fetchExrate() {
         $xml_string = file_get_contents(self::FEED_URL);
         $xml = simplexml_load_string($xml_string, 'SimpleXMLElement', LIBXML_NOCDATA);
         $json = json_encode($xml);
         $array = json_decode($json, true);
-        return $array['channel']['item'];
+        return $array['Exrate'];
     }
 }
